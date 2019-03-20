e97869576895438c02d8c538d546e5d575a4f1f3
Donzulo <mads5376@edu.campusvejle.dk>
2019-03-20 11:00:58 +0100

    Create ClassBiz, fields, properties and empty methods.

---
+++ DanskBiblioteksForening2019/BIZ-DBF(2019)/ClassBiz.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BIZ_DBF_2019_
+{
+    class ClassBiz
+    {
+        // Private fields
+        private ObservableCollection<ClassBog> _boeger;
+        private ObservableCollection<ClassBog> _laanteBoeger;
+        private ClassBog _bog;
+        private ClassPerson _person;
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public ClassBiz()
+        {
+            bog = new ClassBog();
+            person = new ClassPerson();
+        }
+        
+        // Public properties
+        public ClassPerson person
+        {
+            get { return _person; }
+            set { _person = value; }
+        }
+        
+        public ClassBog bog
+        {
+            get { return _bog; }
+            set { _bog = value; }
+        }
+        
+        public ObservableCollection<ClassBog> laanteBoeger
+        {
+            get { return _laanteBoeger; }
+            private set { _laanteBoeger = value; }
+        }
+        
+        public ObservableCollection<ClassBog> boeger
+        {
+            get { return _boeger; }
+            private set { _boeger = value; }
+        }
+
+        public ObservableCollection<ClassBog> GetAllLentBoks(int personID)
+        {
+
+        }
+
+        public ObservableCollection<ClassBog> GetAllBooksWhereTheTitleContainsTheseWords(string words)
+        {
+
+        }
+
+        public void LendThisBookToTheUser(int bogID, int personID)
+        {
+
+        }
+
+        public void SubmitThisBookToTheLibrary(int bogID, int personID)
+        {
+
+        }
+
+        public bool CheckForDoubleLending(ClasBog)
+        {
+
+        }
+    }
+}
