cbc9e49cb424ae2e6aa61ecf99721a0ac44f17ce
Bobby Lejel Gehlert <bobb0038@edu.campusvejle.dk>
2019-03-20 11:36:16 +0100

    Added class ClassLogin, property string id , property string user, Method ClassPerson GetUserData

---
+++ DanskBiblioteksForening2019/BIZ-DBF(2019)/ClassLogin.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BIZ_DBF_2019_
+{
+    class ClassLogin
+    {
+        private string _id;
+        private string _user;
+
+        public ClassLogin()
+        {
+
+        }
+
+        public ClassPerson GetUserData(string id, string user)
+        {
+
+        }
+
+        public string user
+        {
+            get { return _user; }
+            set
+            {
+                if (value != _user)
+                {
+                    _user = value;
+                    //Notify("user");
+                }
+            }
+        }
+
+
+        public string id
+        {
+            get { return _id; }
+            set
+            {
+                if (value != _id)
+                {
+                    _id = value;
+                    //Notify("id");
+                }
+            }
+        }
+
+    }
+}
