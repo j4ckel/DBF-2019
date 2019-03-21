c303a55e8b3ac3f020e544f786f67f010e499e37
MojoDojo <mojofromthedojo@gmail.com>
2019-03-21 10:45:38 +0100

    Har sat Usercontrol ind i main grid

--- DanskBiblioteksForening2019/GUI-DBF(2019)/MainWindow.xaml.cs
+++ DanskBiblioteksForening2019/GUI-DBF(2019)/MainWindow.xaml.cs
@@ -24,8 +24,10 @@ namespace GUI_DBF_2019_
         {
             InitializeComponent();
             UCGUI ucg = new UCGUI();
+            UserControlListView uclv = new UserControlListView();
             //MainGrid.Children.Add(ucg);
             gridRight.Children.Add(ucg);
+            gridLeft.Children.Add(uclv);
         }
     }
 }
