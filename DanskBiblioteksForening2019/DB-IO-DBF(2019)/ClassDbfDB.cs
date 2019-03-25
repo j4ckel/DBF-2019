3060904b873f158be38ca726af6d85dce7e06f83
Christoffer Lorenzen <chri603v@edu.campusvejle.dk>
2019-03-25 11:30:58 +0100

    update

--- DanskBiblioteksForening2019/DB-IO-DBF(2019)/ClassDbfDB.cs
+++ DanskBiblioteksForening2019/DB-IO-DBF(2019)/ClassDbfDB.cs
@@ -64,13 +64,24 @@ namespace DB_IO_DBF_2019_
         public ClassUser GetUser(string cprNr, string Password)
         {
 
-            string strsql = "SELECT        dbo.Person.navn, dbo.Person.adresse, dbo.PersonTelefon.telefonnummer, dbo.PersonMail.mailAdr, " +
+            string strsql = "SELECT dbo.Person.navn, dbo.Person.adresse, dbo.PersonTelefon.telefonnummer, dbo.PersonMail.mailAdr, " +
                 "dbo.Person.rolle FROM dbo.Access LEFT OUTER JOIN" +
-                "                         dbo.Person ON dbo.Access.userId = dbo.Person.id LEFT OUTER JOIN" +
-                "                         dbo.PersonTelefon ON dbo.Person.id = dbo.PersonTelefon.personID LEFT OUTER JOIN" +
-                "                         dbo.PersonMail ON dbo.Person.id = dbo.PersonMail.personID" +
+                " dbo.Person ON dbo.Access.userId = dbo.Person.id LEFT OUTER JOIN" +
+                " dbo.PersonTelefon ON dbo.Person.id = dbo.PersonTelefon.personID LEFT OUTER JOIN" +
+                " dbo.PersonMail ON dbo.Person.id = dbo.PersonMail.personID" +
                 $"WHERE(dbo.Access.cprNr = '{cprNr}') AND(dbo.Access.password = '{Password}')";
 
+            DataTable dt = DbReturnDataTable(strsql);
+            foreach (DataRow row in dt.Rows)
+            {
+                //userName = Cprnr
+                ClassUser CU = new ClassUser();
+                CU.userName = row[""]
+                ClassPerson CP = new ClassPerson();
+
+                    
+
+            }
 
 
             return GetUser(cprNr, Password);
