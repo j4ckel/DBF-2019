3cd38f164f7219e9bd16d1de6a98a20912f2ae08
Bobby Lejel Gehlert <bobb0038@edu.campusvejle.dk>
2019-03-26 11:00:34 +0100

    changes

--- DanskBiblioteksForening2019/DB-IO-DBF(2019)/ClassDbfDB.cs
+++ DanskBiblioteksForening2019/DB-IO-DBF(2019)/ClassDbfDB.cs
@@ -176,16 +176,16 @@ namespace DB_IO_DBF_2019_
 
             return cTitles;
         }
-        public ObservableCollection<ClassAuthors> GetAuthors()
+        public ObservableCollection<ClassAuthor> GetAuthors()
         {
-            ObservableCollection<ClassAuthors> cAuthors = new ObservableCollection<ClassAuthors>();
+            ObservableCollection<ClassAuthor> cAuthors = new ObservableCollection<ClassAuthor>();
             string sqlQuery = "SELECT * FROM Forfatter";
             DataTable dataTable = DbReturnDataTable(sqlQuery);
 
             foreach (DataRow row in dataTable.Rows)
             {
-                ClassAuthors authors = new ClassAuthors();
-                authors.authors = row["forfatter"].ToString();
+                ClassAuthor authors = new ClassAuthor();
+                authors.author = row["forfatter"].ToString();
                 authors.id = row["id"].ToString();
                 cAuthors.Add(authors);
             }
@@ -201,39 +201,39 @@ namespace DB_IO_DBF_2019_
             foreach (DataRow row in dataTable.Rows)
             {
                 ClassISBN isbn = new ClassISBN();
-                isbn.isbn = row["isbnNr"].ToString();
+                isbn.ISBN = row["isbnNr"].ToString();
                 isbn.id = row["id"].ToString();
                 cISBN.Add(isbn);
             }
 
             return cISBN;
         }
-        public ObservableCollection<ClassPublishers> GetPublishers()
+        public ObservableCollection<ClassPublisher> GetPublishers()
         {
-            ObservableCollection<ClassPublishers> cPublisher = new ObservableCollection<ClassPublishers>();
+            ObservableCollection<ClassPublisher> cPublisher = new ObservableCollection<ClassPublisher>();
             string sqlQuery = "SELECT * FROM Forlag";
             DataTable dataTable = DbReturnDataTable(sqlQuery);
 
             foreach (DataRow row in dataTable.Rows)
             {
-                ClassPublishers publishers = new ClassPublishers();
-                publishers.publishers = row["forlagsNavn"].ToString();
+                ClassPublisher publishers = new ClassPublisher();
+                publishers.publisher = row["forlagsNavn"].ToString();
                 publishers.id = row["id"].ToString();
                 cPublisher.Add(publishers);
             }
 
             return cPublisher;
         }
-        public ObservableCollection<ClassTypes> GetTypes()
+        public ObservableCollection<ClassType> GetTypes()
         {
-            ObservableCollection<ClassTypes> cTypes = new ObservableCollection<ClassTypes>();
+            ObservableCollection<ClassType> cTypes = new ObservableCollection<ClassType>();
             string sqlQuery = "SELECT * FROM Type";
             DataTable dataTable = DbReturnDataTable(sqlQuery);
 
             foreach (DataRow row in dataTable.Rows)
             {
-                ClassTypes types = new ClassTypes();
-                types.types = row["TypeNavn"].ToString();
+                ClassType types = new ClassType();
+                types.type = row["TypeNavn"].ToString();
                 types.id = row["id"].ToString();
                 cTypes.Add(types);
             }
