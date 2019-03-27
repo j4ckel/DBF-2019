3c7dba2193a265376804968274be67cb3df413f3
Bobby Lejel Gehlert <bobb0038@edu.campusvejle.dk>
2019-03-27 13:23:02 +0100

    changes

--- DanskBiblioteksForening2019/REPO-DBF(2019)/ClassBog.cs
+++ DanskBiblioteksForening2019/REPO-DBF(2019)/ClassBog.cs
@@ -9,7 +9,7 @@ namespace REPO_DBF_2019_
     /// <summary>
     /// This Class Handles the fields and properties for each book.
     /// </summary>
-    public class ClassBog :ClassNotify
+    public class ClassBog : ClassNotify
     {
         /// <summary>
         /// Default Constructor.
